6971eb6c092c9a4959d8862f61f6a22f248b367c
Fabien Crespel <fabien@crespel.net>
2026-08-02 21:35:26 +0200

    chore: update otel semconv

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"go.opentelemetry.io/otel/metric"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.41.0"
 )
 
 type Config struct {
